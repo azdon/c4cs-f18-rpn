02d3ab56ba5e6e218a73c4b59e8c458582b4e1c8
Annika <azdon@umich.edu>
2018-10-26 11:21:54 -0400

    fell behind but passed addition, started subtraction

---
+++ rpn.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+	#stack for calculator
+	stack = []
+
+	#tokenize input
+	tokens = arg.split()
+
+	#proces tokens
+	for token in tokens:
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			val1 = stack.pop()
+			val2 = stack.pop()
+			result = val1 + val2
+			stack.append(result)
+			return stack[0]
+
+
+
+
+def main():
+	while True:
+		result = calculate(input("rpn calc> "))
+		print results
+
+
+if __name__ == '__main__': 
+	main()
